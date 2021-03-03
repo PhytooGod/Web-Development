bd8383ef7f1f80edf6d7f36b493621447123514f
Phytoo <52602080+PhytooGod@users.noreply.github.com>
2021-03-03 10:39:21 +0500

    Add files via upload

---
+++ lab5/src/app/product-list/product-list.component.ts
@@ -0,0 +1,27 @@
+import { Component } from "@angular/core";
+import { Input } from "@angular/core";
+@Component({
+  selector: "app-product-list",
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"]
+})
+export class ProductListComponent {
+  @Input() product;
+  @Input() category;
+  constructor() {}
+
+  ngOnInit() {}
+  share(url: string) {
+    window.open(url);
+  }
+
+  onNotify() {
+    window.alert("You will be notified when the product goes on sale");
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
---
+++ lab5/src/app/products.ts
@@ -0,0 +1,284 @@
+export const products = [
+  {
+    category_id: 0,
+    category_name: "Games",
+    display: false,
+    items: [
+      {
+        id: 0,
+        name: "Far Cry 5",
+        description:
+          "Far Cry 5 is a 2018 first-person shooter game developed by Ubisoft Montreal and Ubisoft Toronto and published by Ubisoft. It is the fifth main installment in the Far Cry series.",
+        image:
+          "https://images-na.ssl-images-amazon.com/images/I/81Y3qfHlIUL._SL1500_.jpg",
+        rating: "3.0/5",
+        price: "23.80",
+        link:
+          "https://www.amazon.com/Far-Cry-5-PS4-playstation-4/dp/B07L3YGJMY/ref=sr_1_2?dchild=1&keywords=far+cry+5&qid=1614107547&sr=8-2",
+        likes: 1
+      },
+      {
+        id: 1,
+        name: "Horizon Zero Dawn",
+        description:
+          "Horizon Zero Dawn is a 2017 action role-playing game developed by Guerrilla Games and published by Sony Interactive Entertainment. The plot follows Aloy, a young hunter in a world overrun by machines, who sets out to uncover her past. The player uses ranged weapons, a spear, and stealth to combat mechanical creatures and other enemy forces. A skill tree provides the player with new abilities and bonuses. The player can explore the open world to discover locations and take on side quests. It was released for the PlayStation 4 in 2017 and Microsoft Windows in 2020.",
+        image:
+          "https://images-na.ssl-images-amazon.com/images/I/81ED9VvfBwL._SL1500_.jpg",
+        rating: "5/5",
+        price: "19.90",
+        link:
+          "https://www.amazon.com/Horizon-Zero-Dawn-Complete-Hits-PlayStation/dp/B07STWQ38X/ref=sr_1_1?dchild=1&keywords=horizon+zero+dawn&qid=1614107848&sr=8-1",
+        likes: 2
+      },
+      {
+        id: 2,
+        name: "Days Gone",
+        description:
+          "Days Gone is a 2019 action-adventure survival horror video game developed by Bend Studio and published by Sony Interactive Entertainment for the PlayStation 4.",
+        image:
+          "https://images-na.ssl-images-amazon.com/images/I/91hgsxRtZGL._SL1500_.jpg",
+        rating: "4/5",
+        price: "24.99",
+        link:
+          "https://www.amazon.com/Days-Gone-Playstation-4/dp/B01GW8XRCG/ref=sr_1_1?dchild=1&keywords=Days+gone&qid=1614108129&sr=8-1",
+        likes: 3
+      },
+      {
+        id: 3,
+        name: "The Last of Us",
+        description:
+          "The Last of Us is a 2013 action-adventure game developed by Naughty Dog and published by Sony Computer Entertainment. Players control Joel, a smuggler tasked with escorting a teenage girl, Ellie, across a post-apocalyptic United States. The Last of Us is played from a third-person perspective. Players use firearms and improvised weapons, and can use stealth to defend against hostile humans and cannibalistic creatures infected by a mutated fungus in the genus Cordyceps. In the online multiplayer mode, up to eight players engage in cooperative and competitive gameplay.",
+        image:
+          "https://images-na.ssl-images-amazon.com/images/I/81CrZ8T2nrL._SL1500_.jpg",
+        rating: "3/5",
+        price: "19.99",
+        link:
+          "https://www.amazon.com/Last-Us-Remastered-Hits-PlayStation-4/dp/B07DNDF1D6/ref=pd_sbs_2?pd_rd_w=KPmfR&pf_rd_p=c52600a3-624a-4791-b4c4-3b112e19fbbc&pf_rd_r=5VFPRJ0J4FG2V9AQD3YY&pd_rd_r=a526366c-4f71-4572-b43d-73cda424b0f4&pd_rd_wg=ESdaU&pd_rd_i=B07DNDF1D6&psc=1",
+        likes: 4
+      },
+      {
+        id: 4,
+        name: "The Last of Us 2",
+        description:
+          "The Last of Us Part II is a 2020 action-adventure game developed by Naughty Dog and published by Sony Interactive Entertainment for the PlayStation 4. Set five years after The Last of Us (2013), the game focuses on two playable characters in a post-apocalyptic United States whose lives intertwine: Ellie, who sets out for revenge after suffering a tragedy, and Abby, a soldier who becomes involved in a conflict between her militia and a religious cult. The game is played from the third-person perspective and allows the player to fight human enemies and cannibalistic zombie-like creatures with firearms, improvised weapons, and stealth.",
+        image:
+          "https://upload.wikimedia.org/wikipedia/en/4/4f/TLOU_P2_Box_Art_2.png",
+        rating: "3/5",
+        price: "19.99",
+        link:
+          "https://www.amazon.com/Last-Us-Remastered-Hits-PlayStation-4/dp/B07DNDF1D6/ref=pd_sbs_2?pd_rd_w=KPmfR&pf_rd_p=c52600a3-624a-4791-b4c4-3b112e19fbbc&pf_rd_r=5VFPRJ0J4FG2V9AQD3YY&pd_rd_r=a526366c-4f71-4572-b43d-73cda424b0f4&pd_rd_wg=ESdaU&pd_rd_i=B07DNDF1D6&psc=1",
+        likes: 5
+      }
+    ]
+  },
+  {
+    category_id: 1,
+    category_name: "Phones",
+    display: false,
+    items: [
+      {
+        id: 0,
+        name: "Samsung Galaxy A31",
+        description:
+          "Making sense of Samsung's Galaxy A and M series models is getting harder by the month, as new models are brought in with minor refreshes, but don't always line up in a logical order. For example, the very recent Galaxy M21 (Review) was basically a Galaxy M30s (Review) with a different selfie camera, but going by the model names, it's impossible to make that connection.",
+        image:
+          "https://images-na.ssl-images-amazon.com/images/I/61A3UNuWO9L._AC_SL1500_.jpg",
+        rating: "2/5",
+        price: "209.99",
+        link:
+          "https://www.amazon.com/Samsung-Galaxy-A31-128GB-4GB-International/dp/B087CBZPX6/ref=sr_1_5?dchild=1&keywords=phone&qid=1614747840&sr=8-5",
+        likes: 4
+      },
+      {
+        id: 1,
+        name: "Samsung Galaxy S20 FE 5G",
+        description:
+          "Pro-Grade Camera: The new Samsung Galaxy S20 FE mobile phone features high-powered pro lenses for beautiful portraits, stunning landscapes and crisp close-ups in any light with its 3X optical zoom",
+        image:
+          "https://images-na.ssl-images-amazon.com/images/I/71z2dc%2BXnrL._AC_SL1500_.jpg",
+        rating: "2/5",
+        price: "325",
+        link:
+          "https://www.amazon.com/Samsung-Factory-Unlocked-Smartphone-Pro-Grade/dp/B08FYTSXGQ/ref=sr_1_6?dchild=1&keywords=phone&qid=1614747840&sr=8-6",
+        likes: 25
+      },
+      {
+        id: 2,
+        name: "Google Pixel 4a",
+        description:
+          "Capture great photos using your cell phone on the 12 MP dual pixel rear camera with features like Live HDR+, Night Sight, and Portrait Mode; share photos directly from the viewfinder of your Pixel camera to Google and popular third party apps",
+        image:
+          "https://images-na.ssl-images-amazon.com/images/I/7199N-Uz2AL._AC_SL1500_.jpg",
+        rating: "5/5",
+        price: "200",
+        link:
+          "https://www.amazon.com/Google-Pixel-4a-Unlocked-Smartphone/dp/B08CFSZLQ4/ref=sr_1_8?dchild=1&keywords=phone&qid=1614747840&sr=8-8",
+        likes: 52
+      },
+      {
+        id: 3,
+        name: "Samsung Galaxy A10s",
+        description:
+          "Compatible with Most GSM Carriers like T-Mobile, AT&T, MetroPCS, etc. Will NOT work with CDMA Carriers Such as Verizon, Sprint, Boost International Model - No warranty in US.",
+        image:
+          "https://images-na.ssl-images-amazon.com/images/I/51ME-ADMjRL._AC_SL1000_.jpg",
+        rating: "2/5",
+        price: "350",
+        link:
+          "https://www.amazon.com/Samsung-Galaxy-A10s-A107M-Infinity-V/dp/B07XQHM1CV/ref=sr_1_12?dchild=1&keywords=phone&qid=1614747840&sr=8-12",
+        likes: 2
+      },
+      {
+        id: 4,
+        name: "Samsung Galaxy A01",
+        description:
+          "Dual 13MP + 2MP Camera/5MP Front Facing Camera; Internal Memory 16 GB (device only); Supports microSD Memory Card up to 512GB (not included)",
+        image:
+          "https://images-na.ssl-images-amazon.com/images/I/61gxAIP-W5L._AC_SL1500_.jpg",
+        rating: "3/5",
+        price: "450",
+        link:
+          "https://www.amazon.com/Simple-Mobile-Samsung-Prepaid-Smartphone/dp/B088N774B7/ref=sr_1_15?dchild=1&keywords=phone&qid=1614747840&sr=8-15",
+        likes: 0
+      }
+    ]
+  },
+  {
+    category_id: 2,
+    category_name: "Bra",
+    display: false,
+    items: [
+      {
+        id: 0,
+        name: "RUNNING GIRL Sports Bra for Women",
+        description:
+          "Need a sports bra that does it all? Look no further! This sweat-wicking cross-strap bra gives you the coverage and support you need for yoga, run, and the gym—without restricting your movement or breath. fabric has added lycra that allows the bra to stretch with you and retain its shape over time. Slip in optional, removable cups for full coverage—if you want it. This bra is intended to provide medium support for great shape retention, long-lasting comfort.",
+        image:
+          "https://images-na.ssl-images-amazon.com/images/I/81pwOagjybL._AC_UX569_.jpg",
+        rating: "5/5",
+        price: "22.99",
+        link:
+          "https://www.amazon.com/RUNNING-GIRL-Criss-Cross-Removable-WX2353-Black-XL/dp/B07XBJ6QVR/ref=sr_1_10?dchild=1&keywords=clothes&qid=1614748719&sr=8-10&th=1",
+        likes: 23
+      },
+      {
+        id: 1,
+        name: "Lemedy Women Padded Sports Bra",
+        description:
+          "Padded sport bra with removable pads for convenient adjustment, with a cotton-like material, soft and comfortable to wear. For yoga,exercise,fitness,any type of workout,or everyday use. Lemedy Yoga bra combine fashion,function and performance",
+        image:
+          "https://images-na.ssl-images-amazon.com/images/I/61pl1AAVtGL._AC_UX385_.jpg",
+        rating: "3/5",
+        price: "23.99",
+        link:
+          "https://www.amazon.com/Lemedy-Padded-Fitness-Workout-Running/dp/B085L9LK8J/ref=sr_1_5?dchild=1&keywords=clothes&qid=1614748719&sr=8-5",
+        likes: 24
+      },
+      {
+        id: 2,
+        name: "Lemedy Women Padded Sports Bra",
+        description: "Intended to provide Light support for a A/B/C cup.",
+        image:
+          "https://images-na.ssl-images-amazon.com/images/I/716q-vC92%2BL._AC_UX385_.jpg",
+        rating: "3/5",
+        price: "23.99",
+        link:
+          "https://www.amazon.com/Lemedy-Padded-Fitness-Workout-Running/dp/B07TWLP3M8/ref=sxin_10_ac_d_rm?ac_md=1-1-Y2xvdGhlcyB3b21lbg%3D%3D-ac_d_rm&cv_ct_cx=clothes&dchild=1&keywords=clothes&pd_rd_i=B07TWLP3M8&pd_rd_r=fe3b4463-241a-4c6e-ac17-6f378ead0477&pd_rd_w=v3AMR&pd_rd_wg=5vuVQ&pf_rd_p=5ceb2a76-ceaa-45a9-982b-6fa61a20d67b&pf_rd_r=6FSJ8529Y93DN1DFXX68&psc=1&qid=1614748719&sr=1-1-12d4272d-8adb-4121-8624-135149aa9081",
+        likes: 25
+      },
+      {
+        id: 3,
+        name: "Lemedy Women Sports Bra Longline Crop",
+        description:
+          "Padded sport bra with removable pads for convenient adjustment, with a cotton-like material, soft and comfortable to wear. Lemedy Yoga bra combine fashion, function and performance.",
+        image:
+          "https://images-na.ssl-images-amazon.com/images/I/61lL3xPom-L._AC_UX569_.jpg",
+        rating: "3/5",
+        price: "23.99",
+        link:
+          "https://www.amazon.com/Lemedy-Sports-Longline-Workout-Running/dp/B08H515QD5/ref=sr_1_1?dchild=1&keywords=clothes&qid=1614748719&sr=8-1",
+        likes: 26
+      },
+      {
+        id: 4,
+        name: "Calvin Klein Women's Modern Cotton Bralette",
+        description: "53% Cotton, 35% Modal, 12% Elastane",
+        image:
+          "https://images-na.ssl-images-amazon.com/images/I/71CvUMVyk3L._AC_UX522_.jpg",
+        rating: "4/5",
+        price: "60.00",
+        link:
+          "https://www.amazon.com/Calvin-Klein-Womens-Regular-Bralette/dp/B06X93XMQR/ref=sr_1_4?dchild=1&keywords=clothes&qid=1614748719&sr=8-4",
+        likes: 27
+      }
+    ]
+  },
+  {
+    category_id: 3,
+    category_name: "Sneakers",
+    display: false,
+    items: [
+      {
+        id: 0,
+        name: "adidas Women's Grand Court Sneaker",
+        description: "100% Other fibers",
+        image:
+          "https://images-na.ssl-images-amazon.com/images/I/71qdoDlEOpL._AC_UX695_.jpg",
+        rating: "4/5",
+        price: "138.28",
+        link:
+          "https://www.amazon.com/adidas-Womens-Grand-Court-Black/dp/B07DBHRKL9/ref=sr_1_6?dchild=1&keywords=sneakers&qid=1614749264&sr=8-6",
+        likes: 50
+      },
+      {
+        id: 1,
+        name:
+          "Amazon Brand - 206 Collective Women's Carla Lace Up Casual Sneakers",
+        description: "100% Cotton Canvas",
+        image:
+          "https://images-na.ssl-images-amazon.com/images/I/61XYfAt-2TL._AC_UX695_.jpg",
+        rating: "3/5",
+        price: "26.09",
+        link:
+          "https://www.amazon.com/206-Collective-Womens-Carla-Canvas/dp/B07MWZ1HSH/ref=sr_1_1_sspa?dchild=1&keywords=sneakers&qid=1614749264&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEySThTRlc4ODhCMDhIJmVuY3J5cHRlZElkPUEwNzY5NzYwMzJHVVlONE03UDkyMyZlbmNyeXB0ZWRBZElkPUEwMDEyNzg0VjVCRElYN0NIOTU2JndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==",
+        likes: 55
+      },
+      {
+        id: 2,
+        name: "New Balance Women's FuelCore Nergize V1 Sneaker",
+        description: "100% Synthetic",
+        image:
+          "https://images-na.ssl-images-amazon.com/images/I/71GyzXlZlRL._AC_UX695_.jpg",
+        rating: "2/5",
+        price: "140.26",
+        link:
+          "https://www.amazon.com/New-Balance-Nergize-FuelCore-Sneaker/dp/B07C2JXT2G/ref=sr_1_2?dchild=1&keywords=sneakers&qid=1614749264&sr=8-2",
+        likes: 60
+      },
+      {
+        id: 3,
+        name: "adidas Women's Cloudfoam Pure Running Shoe",
+        description: "Rubber sole",
+        image:
+          "https://images-na.ssl-images-amazon.com/images/I/81zc8i%2B8qfL._AC_UX695_.jpg",
+        rating: "2/5",
+        price: "155.50",
+        link:
+          "https://www.amazon.com/adidas-Performance-Womens-Cloudfoam-Running/dp/B0711R2TNB/ref=sr_1_3?dchild=1&keywords=sneakers&qid=1614749264&sr=8-3",
+        likes: 66
+      },
+      {
+        id: 4,
+        name: "PUMA Women's Carina Sneaker",
+        description: "Leather and Synthetic",
+        image:
+          "https://images-na.ssl-images-amazon.com/images/I/71FfObARk-L._AC_UX695_.jpg",
+        rating: "4/5",
+        price: "109.00",
+        link:
+          "https://www.amazon.com/PUMA-Womens-Carina-Sneaker-Silver/dp/B07HJLYCJT/ref=sr_1_4?dchild=1&keywords=sneakers&qid=1614749264&sr=8-4",
+        likes: 77
+      }
+    ]
+  }
+];
