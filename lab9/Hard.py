a1d70002e637c8403dcbe9f4d561ff4f000b6b98
Phytoo <52602080+PhytooGod@users.noreply.github.com>
2021-04-04 14:51:11 +0500

    Update Hard.py

--- lab9/Hard.py
+++ lab9/Hard.py
@@ -4,17 +4,30 @@ m = int(c[1])
 a = []
 for i in range(n):
     a.append([])
+    for j in range(m):
+        a[i].append(' ')
+for i in range(n):
     b = input()
     for j in range(m):
-        a[i].append(b[j])
+        a[i][j] = b[j]
 k = []
 l = []
 for j in range(m):
     for i in range(n):
         k.append(a[i][j].isalnum())
         l.append(a[i][j])
-while k[-1] == False:
-    k.pop(-1)
+lol=0
+try:
+    while k[-1] == False:
+        k.pop(-1)
+except:
+    pass
+try:
+    while k[0] == False:
+        k.pop(0)
+        lol+=1
+except:
+    pass
 indexes = []
 offset = -1
 while True:
@@ -22,7 +35,7 @@ while True:
         offset = k.index(False, offset+1)
     except ValueError:
         break
-    indexes.append(offset)
+    indexes.append(offset+lol)
 for i in indexes:
     l[i] = ' '
 print(*l, sep='')
